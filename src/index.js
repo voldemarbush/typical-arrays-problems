b0fdd03355445974b4734fbb2bcba8d02d4a8b66
Bushev Vladimir <voldemarbush@gmail.com>
2021-08-24 00:21:52 +0300

    feat: make project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+    let ln = (array) ? array.length : 0;
+    if (array && ln != 0) {
+        let res = array[0];
+        for (let a = 1; a <= ln - 1; a++) {
+            res = (array[a] < res) ? array[a] : res;
+        }
+        return res;
+    } else return 0;    
 }
 
 exports.max = function max (array) {
-  return 0;
+    let ln = (array) ? array.length : 0;
+    if (ln != 0) {
+        let res = array[0];
+        for (let a = 1; a <= ln - 1; a++) {
+            res = (array[a] > res) ? array[a] : res;
+        }
+        return res;
+    } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let ln = (array) ? array.length : 0;
+    let res = 0;
+    if (array && ln != 0) {
+        for (let a = 0; a <= ln - 1; a++) {
+            res += array[a];
+        }
+        return res / ln;
+    } else return 0;
 }
